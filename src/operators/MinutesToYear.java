33d122f48cc8506645ef03c343943f074f6bb723
SenolDemir <66261119+SenolDemir@users.noreply.github.com>
2024-05-02 17:07:17 +0200

    Update MinutesToYear.java

--- src/operators/MinutesToYear.java
+++ src/operators/MinutesToYear.java
@@ -6,10 +6,17 @@ public class MinutesToYear {
 
         // 3456789
 
-        int year =  3456789/60/24/365;
-        int days =  (3456789/60/24)%365;
+       int min = 3456789;
+        int minBalance = min%60;
+        System.out.println(minBalance);
+        int hour = (min-minBalance) / 60;
+        int hourBalance = hour%24;
+        int day = (hour-hourBalance) / 24;
+        System.out.println(day);
+        int dayBalance = day%365;
+        int year = (day-dayBalance)/365;
+        System.out.println(year);
+        System.out.println(min + " minutes is: " + year + " years " + dayBalance + " days " + hourBalance +" hours " + minBalance + " minutes");
 
-        System.out.println("year = " + year);
-        System.out.println("days = " + days);
     }
 }
